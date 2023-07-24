8e3ad5c7c34a60a1df75b730658b2fe843041320
TerrTerr <terrywithers@gmail.com>
2023-07-23 22:59:59 -0400

    new site

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.getElementById('generateButton').addEventListener('click', function() {
-    const suggestions = ['banana', 'airplane', 'violin', 'sandwich', 'butterfly', 
-                         'parade', 'robot', 'rainbow', 'unicorn', 'volcano', 
-                         'museum', 'elephant', 'spaceship', 'mermaid', 'wizard'];
-    
-    const randomIndex = Math.floor(Math.random() * suggestions.length);
-    const randomSuggestion = suggestions[randomIndex];
-
-    document.getElementById('suggestion').innerText = randomSuggestion;
+    fetch('https://www.mit.edu/~ecprice/wordlist.10000')
+        .then(response => response.text())
+        .then(data => {
+            const suggestions = data.split('\n'); // split the text into an array of lines/words
+            
+            const randomIndex = Math.floor(Math.random() * suggestions.length);
+            const randomSuggestion = suggestions[randomIndex];
+            
+            document.getElementById('suggestion').innerText = randomSuggestion;
+        })
+        .catch(error => console.error('Error:', error));
 });
