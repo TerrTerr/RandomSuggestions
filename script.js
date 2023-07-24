88ad9096f2aaf02e2e36e53c2eafc07833b617f0
TerrTerr <terrywithers@gmail.com>
2023-07-23 23:18:07 -0400

    new style

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 document.getElementById('generateButton').addEventListener('click', function() {
-    fetch('https://www.ef.com/wwen/english-resources/english-vocabulary/top-3000-words/')
+    fetch('https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt')
         .then(response => response.text())
         .then(data => {
-            // Parse the HTML and extract the words
-            // This part would be more complex in reality
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(data, 'text/html');
-            const wordElements = doc.querySelectorAll('.word');
-            const suggestions = Array.from(wordElements).map(el => el.textContent);
-            
+            const suggestions = data.split('\n');
+
             const randomIndex = Math.floor(Math.random() * suggestions.length);
             const randomSuggestion = suggestions[randomIndex];
-            
+
             document.getElementById('suggestion').innerText = randomSuggestion;
         })
         .catch(error => console.error('Error:', error));
